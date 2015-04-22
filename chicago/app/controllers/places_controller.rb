a262c62b07c476aece828db33ed835c073983acf
J. Alex Johnson <j.alex.johnson@gmail.com>
2015-04-21 23:08:32 -0500

    Completed all tasks

---
+++ chicago/app/controllers/places_controller.rb
@@ -0,0 +1,55 @@
+class PlacesController < ApplicationController
+	def index
+		@places = Place.all
+	end
+
+	def show
+		@place = Place.find_by(:id => params["id"])
+		@reviews = Review.where(:place_id => params["id"]).order("created desc")
+	end
+
+	def delete
+		@place = Place.find_by(:id => params["id"])
+		@place.delete
+		redirect_to "/", notice: "Place deleted."
+	end
+
+	def new
+	end
+
+	def create
+		p = Place.new
+		p.title = params["title"]
+		p.photo_url = params["photo_url"]
+		p.price = params["price"].to_f * 100
+		p.description = params["description"]
+		p.save
+		redirect_to "/", notice: "Place created."
+	end
+
+	def edit
+		@place = Place.find_by(:id => params["id"])
+	end
+
+	def update
+		@place = Place.find_by(:id => params["id"])
+		@place.title = params["title"]
+		@place.photo_url = params["photo_url"]
+		@place.price = params["price"].to_f * 100
+		@place.description = params["description"]
+		@place.save
+		redirect_to "/places/#{@place.id}", notice: "Place updated."
+	end
+
+	def review
+		r = Review.new
+		r.title = params["title"]
+		r.rating = params["rating"].to_i
+		r.place_id = params["id"].to_i
+		r.description = params["description"]
+		r.created = DateTime.now
+		r.save
+		redirect_to "/places/#{params["id"]}", notice: "Review submitted."
+	end
+
+end
--- chicago/db/seeds.rb
+++ chicago/db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Place.delete_all
+
+p = Place.new
+p.title = "Art Institute of Chicago"
+p.photo_url = "http://cdn.pursuitist.com/wp-content/uploads/2014/09/Art-Institute-of-Chicago-Named-The-World%E2%80%99s-Best-Museum.jpg"
+p.price = 2300
+p.description = "The Art Institute of Chicago (AIC) is an encyclopedic art museum located in Chicago's Grant Park. It features a collection of Impressionist and Post-Impressionist art in its permanent collection. Its holdings also include American art, Old Masters, European and American decorative arts, Asian art, modern and contemporary art, and architecture and industrial and graphic design. In addition, it houses the Ryerson & Burnham Libraries. (source: Wikipedia)"
+p.save
+
+p = Place.new
+p.title = "Cloud Gate (The Bean)"
+p.photo_url = "http://upload.wikimedia.org/wikipedia/en/c/c1/Cloud_Gate_%28The_Bean%29_from_east%27.jpg"
+p.price = 0
+p.description = "Cloud Gate is a public sculpture by Indian-born British artist Anish Kapoor, that is the centerpiece of AT&T Plaza at Millennium Park in the Loop community area of Chicago, Illinois. The sculpture and AT&T Plaza are located on top of Park Grill, between the Chase Promenade and McCormick Tribune Plaza & Ice Rink. Constructed between 2004 and 2006, the sculpture is nicknamed The Bean because of its bean-like shape. Made up of 168 stainless steel plates welded together, its highly polished exterior has no visible seams. It measures 33 by 66 by 42 feet (10 by 20 by 13 m), and weighs 110 short tons (100 t; 98 long tons). (source: wikipedia)"
+p.save
+
+p = Place.new
+p.title = "Millennium Park"
+p.photo_url = "http://upload.wikimedia.org/wikipedia/commons/thumb/5/50/2005-10-13_2880x1920_chicago_above_millennium_park.jpg/1024px-2005-10-13_2880x1920_chicago_above_millennium_park.jpg"
+p.price = 0
+p.description = "Millennium Park is a public park located in the Loop community area of Chicago in Illinois, US, and originally intended to celebrate the millennium. It is a prominent civic center near the city's Lake Michigan shoreline that covers a 24.5-acre (99,000 m2) section of northwestern Grant Park. The area was previously occupied by parkland, Illinois Central rail yards, and parking lots.[1] The park, which is bounded by Michigan Avenue, Randolph Street, Columbus Drive and East Monroe Drive, features a variety of public art. As of 2009, Millennium Park trailed only Navy Pier as a Chicago tourist attraction. (source: wikipedia)"
+p.save
+
+p = Place.new
+p.title = "Chicago Architecture Foundation River Cruise"
+p.photo_url = "http://s3.amazonaws.com/architecture-org/files/events/chicago_architecture_foundation_river_cruise_chica-2.jpg"
+p.price = 3974
+p.description = "Voted “Chicago’s Best Tour” by Chicago Reader, the CAF River Cruise is a “must” for out-of-towners and Chicagoans alike. CAF-certified volunteer tour guides—called docents—interpret more than 50 buildings along the Chicago River, revealing how the city grew from a small back-country outpost into one of the world’s most important crossroads in less than 100 years. In 90 minutes, get the real story on Chicago architecture and its history. (souce: architecture.org)"
+p.save
+
+p = Place.new
+p.title = "Museum of Science and Industry"
+p.photo_url = "http://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/MSIChicago.JPG/1024px-MSIChicago.JPG"
+p.price = 1800
+p.description = "The Museum of Science and Industry (MSI) is located in Chicago, Illinois, in Jackson Park, in the Hyde Park neighborhood between Lake Michigan and The University of Chicago. It is housed in the former Palace of Fine Arts from the 1893 World's Columbian Exposition. Initially endowed by Julius Rosenwald, the Sears, Roebuck and Company president and philanthropist, it was supported by the Commercial Club of Chicago and opened in 1933 during the Century of Progress Exposition. (source: Wikipedia)"
+p.save
